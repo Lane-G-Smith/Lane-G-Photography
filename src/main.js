270a6467380f05da144ff942812e14294ef71f1f
Lane G Smith <lanegphotography@gmail.com>
2023-03-29 12:56:04 -0700

    create new project

---
+++ src/main.js
@@ -0,0 +1,3 @@
+// open and close modals
+
+// 
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        dgreen: "#2C3333",
+        lgreen: "#C4D8C2",
+        dpeach: "#A27B5C",
+        lpeach: "#C36839",
+        lgrey: "#F2F2F2",
+        dgrey: "#BDBDBD",
+      },
+    },
+    plugins: [],
+  }
+}
