6390fd444805f64abea12de31d4c8ee629fd294e
Lane G Smith <lane.smith.contact@gmail.com>
2023-08-07 16:24:31 -0700

    create framework delete old pages update file structure add burger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./styles/**.css", "./**{.html,.js,.css}", "./pages/**{.html,.js,.css}"],
+  content: ["./styles/*.css", "./**/*{.html,.js,.css}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    extend: {
-      colors: {
-        dgreen: "#2C3333",
-        lgreen: "#C4D8C2",
-        dpeach: "#A27B5C",
-        lpeach: "#C36839",
-        lgrey: "#F2F2F2",
-        dgrey: "#BDBDBD",
-      },
-    },
+    extend: {},
     plugins: [],
   }
 }
